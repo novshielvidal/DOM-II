01275d5465837cf0eb8b51e038c32edf0602ca81
Novshiel Vidal <26637610+novshielvidal@users.noreply.github.com>
2020-01-21 19:30:40 -0800

    Transform size of nav-link during click and mouseout

--- js/index.js
+++ js/index.js
@@ -10,7 +10,15 @@ navLinks.forEach(navLink => {
 
 //  Event Listener 2
 navLinks.forEach(navLink => {
+    navLink.addEventListener('click', (event) => {
+        event.target.style.transform = "scale(1.5)";
+    });
+});
+
+//  Event Listener 3
+navLinks.forEach(navLink => {
     navLink.addEventListener('mouseout', (event) => {
         event.target.style.color = '#212529';
+        event.target.style.transform = "scale(1.0)";
     });
 });
