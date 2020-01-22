57792a16d71469c95b2a5bb7d14ee7d2f4ecad9d
Novshiel Vidal <26637610+novshielvidal@users.noreply.github.com>
2020-01-21 20:08:15 -0800

    Create drag and dragend event listeners on img

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-//  Event Listener 1
+//  Event Listener 1 - mouseover
 const navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(navLink => {
     navLink.addEventListener('mouseover', (event) => {
@@ -8,17 +8,46 @@ navLinks.forEach(navLink => {
     });
 });
 
-//  Event Listener 2
+//  Event Listener 2 - click
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', (event) => {
         event.target.style.transform = "scale(1.5)";
     });
 });
 
-//  Event Listener 3
+//  Event Listener 3 - mouseout
 navLinks.forEach(navLink => {
     navLink.addEventListener('mouseout', (event) => {
         event.target.style.color = '#212529';
         event.target.style.transform = "scale(1.0)";
     });
 });
+
+//  Event Listener 4 - drag
+const images = document.querySelectorAll('img');
+    images.forEach(image => {
+        image.addEventListener('drag', (event) => {
+            event.target.style.transform = "scale(2.0)";
+        });
+    });
+console.log(images);
+
+//  Event Listener 5 - dragend
+images.forEach(image => {
+    image.addEventListener('dragend', (event) => {
+        event.target.style.transform = "scale(1.0)";
+    });
+});
+
+//  Create dropzone
+const dropZone = document.createElement('div');
+    document.body.appendChild(dropZone);
+console.log(dropZone);
+
+//  Event Listener 6 - dragenter
+// images.forEach(image => {
+//     image.addEventListener{'dragenter', (enter) => {
+
+//     }}
+// })
+
