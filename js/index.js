ee240eace10be6a14d24b669f58c02c799b11e89
Novshiel Vidal <26637610+novshielvidal@users.noreply.github.com>
2020-01-21 19:21:47 -0800

    Create event listener to change color of nav-link during mouseover and mouseout.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//  Event Listener 1
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(navLink => {
+    navLink.addEventListener('mouseover', (event) => {
+        event.target.style.color = "blue";
+    });
+});
+
+//  Event Listener 2
+navLinks.forEach(navLink => {
+    navLink.addEventListener('mouseout', (event) => {
+        event.target.style.color = '#212529';
+    });
+});
---
+++ stretch-assignment/index.js
@@ -0,0 +1,2 @@
+const links = document.querySelectorAll('a');
+console.log(links);
